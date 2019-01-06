0224af632dd5c8b51973c4c81f16ed40377080b2
Luke Granger <grangerl330@gmail.com>
2019-01-06 23:45:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
     lowest_value = nil
     lowest_key = nil
@@ -12,3 +9,5 @@ def key_for_min_value(name_hash)
   end
   lowest_key
 end
+
+
