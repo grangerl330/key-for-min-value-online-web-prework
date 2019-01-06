d39e63e1d56e7009f56544095676bae85502bf05
Luke Granger <grangerl330@gmail.com>
2019-01-06 23:45:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
     lowest_value = nil
     lowest_key = nil
   name_hash.collect do |key, value|
-    if lowest_value == nil || value < lowest_value
+    if lowest_value == nil or value < lowest_value
       lowest_value = value
       lowest_key = key
     end
